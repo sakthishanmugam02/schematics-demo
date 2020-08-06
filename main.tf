ed93b15466ca95ce75292e47e22bba93797388a0
sakthishanmugam02 <35956665+sakthishanmugam02@users.noreply.github.com>
2020-08-06 20:44:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "ibm_dns_resource_record" "test_schematics_demo_pdns_record_a" {
   zone_id     = ibm_dns_zone.test_schematics_demo_pdns_zone.zone_id
   type        = "A"
   name        = "testA"
-  rdata       = ibm_is_instance.test_schematics_demo_vsi_client.network_interfaces.primary_ipv4_address
+  rdata       = ibm_is_instance.test_schematics_demo_vsi_client.network_interfaces[0].primary_ipv4_address
   ttl         = 900
 }
 
