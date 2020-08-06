deb796f0c2ef54552a8eb2edb026ecb67d6cf22f
sakthishanmugam02 <35956665+sakthishanmugam02@users.noreply.github.com>
2020-08-06 22:53:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
+###
+#1 Setting the provider plugin for Terraform i.e. IBM
+###
+
 provider "ibm" {
   generation = 2
   region     = "us-south"
 }
 
+###
+#2 Create a VPC
+###
+
 data "ibm_resource_group" "rg" {
-  name = "Default"
-}
-
-data "ibm_is_image" "test_schematics_demo_image" {
-  name = "ibm-ubuntu-18-04-1-minimal-amd64-2"
-}
-
-data "ibm_is_ssh_key" "test_schematics_demo_ssh_key" {
-  name = "sakthi-ssh"
+  name = "Demo Related"
 }
 
 resource "ibm_is_vpc" "test_schematics_demo_vpc" {
@@ -21,40 +21,9 @@ resource "ibm_is_vpc" "test_schematics_demo_vpc" {
   resource_group = data.ibm_resource_group.rg.id
 }
 
-resource "ibm_resource_instance" "test_schematics_demo_pdns" {
-  depends_on        = [ibm_is_vpc.test_schematics_demo_vpc]
-  name              = "test-schematics-demo-pdns"
-  resource_group_id = data.ibm_resource_group.rg.id
-  location          = "global"
-  service           = "dns-svcs"
-  plan              = "standard-dns"
-}
-
-resource "ibm_dns_zone" "test_schematics_demo_pdns_zone" {
-  depends_on  = [ibm_resource_instance.test_schematics_demo_pdns]
-  name        = "test.com"
-  instance_id = ibm_resource_instance.test_schematics_demo_pdns.guid
-  description = "testdescription"
-  label       = "testlabel"
-}
-
-resource "ibm_dns_permitted_network" "test_schematics_demo_pdns_permitted_network" {
-  depends_on  = [ibm_dns_zone.test_schematics_demo_pdns_zone]
-  instance_id = ibm_resource_instance.test_schematics_demo_pdns.guid
-  zone_id     = ibm_dns_zone.test_schematics_demo_pdns_zone.zone_id
-  vpc_crn     = ibm_is_vpc.test_schematics_demo_vpc.resource_crn
-}
-
-resource "ibm_dns_resource_record" "test_schematics_demo_pdns_record_a" {
-  depends_on  = [ibm_dns_permitted_network.test_schematics_demo_pdns_permitted_network, ibm_is_instance.test_schematics_demo_vsi_server]
-  instance_id = ibm_resource_instance.test_schematics_demo_pdns.guid
-  zone_id     = ibm_dns_zone.test_schematics_demo_pdns_zone.zone_id
-  type        = "A"
-  name        = "testA"
-  rdata       = ibm_is_instance.test_schematics_demo_vsi_server.primary_network_interface[0].primary_ipv4_address
-  #rdata = "1.2.3.4"
-  ttl         = 900
-}
+###
+#3 Create a Subnet for the VPC VSI resources
+###
 
 resource "ibm_is_subnet" "test_schematics_demo_subnet" {
   name            = "test-schematics-demo-subnet"
@@ -62,6 +31,10 @@ resource "ibm_is_subnet" "test_schematics_demo_subnet" {
   zone            = "us-south-1"
   ipv4_cidr_block = "10.240.0.0/24"
 }
+
+###
+#4 Create a Server & the Client with required the Security Group rules
+###
 
 resource "ibm_is_security_group" "test_schematics_demo_sg" {
   name           = "test-schematics-demo-sg"
@@ -92,6 +65,14 @@ resource "ibm_is_security_group_rule" "test_schematics_demo_sg_rule_all_out" {
   group      = ibm_is_security_group.test_schematics_demo_sg.id
   direction  = "outbound"
   remote     = "0.0.0.0/0"
+}
+
+data "ibm_is_image" "test_schematics_demo_image" {
+  name = "ibm-ubuntu-18-04-1-minimal-amd64-2"
+}
+
+data "ibm_is_ssh_key" "test_schematics_demo_ssh_key" {
+  name = "sakthi-ssh"
 }
 
 resource "ibm_is_instance" "test_schematics_demo_vsi_client" {
@@ -154,6 +135,44 @@ resource "ibm_is_instance" "test_schematics_demo_vsi_server" {
   }
 }
 
+###
+#5 Provision Private DNS for the VPC that is created in the above steps
+###
+
+resource "ibm_resource_instance" "test_schematics_demo_pdns" {
+  depends_on        = [ibm_is_vpc.test_schematics_demo_vpc]
+  name              = "test-schematics-demo-pdns"
+  resource_group_id = data.ibm_resource_group.rg.id
+  location          = "global"
+  service           = "dns-svcs"
+  plan              = "standard-dns"
+}
+
+resource "ibm_dns_zone" "test_schematics_demo_pdns_zone" {
+  depends_on  = [ibm_resource_instance.test_schematics_demo_pdns]
+  name        = "test.com"
+  instance_id = ibm_resource_instance.test_schematics_demo_pdns.guid
+  description = "testdescription"
+  label       = "testlabel"
+}
+
+resource "ibm_dns_permitted_network" "test_schematics_demo_pdns_permitted_network" {
+  depends_on  = [ibm_dns_zone.test_schematics_demo_pdns_zone]
+  instance_id = ibm_resource_instance.test_schematics_demo_pdns.guid
+  zone_id     = ibm_dns_zone.test_schematics_demo_pdns_zone.zone_id
+  vpc_crn     = ibm_is_vpc.test_schematics_demo_vpc.resource_crn
+}
+
+resource "ibm_dns_resource_record" "test_schematics_demo_pdns_record_a" {
+  depends_on  = [ibm_dns_permitted_network.test_schematics_demo_pdns_permitted_network, ibm_is_instance.test_schematics_demo_vsi_server]
+  instance_id = ibm_resource_instance.test_schematics_demo_pdns.guid
+  zone_id     = ibm_dns_zone.test_schematics_demo_pdns_zone.zone_id
+  type        = "A"
+  name        = "testA"
+  rdata       = ibm_is_instance.test_schematics_demo_vsi_server.primary_network_interface[0].primary_ipv4_address
+  #rdata = "1.2.3.4"
+  ttl         = 900
+}
 
 resource "ibm_is_floating_ip" "test_schematics_demo_fip" {
   name   = "test-schematics-demo-fip"
